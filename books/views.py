2ae94f112bf348f54616ffdca2f73196e6c7d220
SheraliJurayev <sheralijurayev0412@gmail.com>
2023-03-16 19:01:18 +0500

    create template booklisthtml classes and urls

--- books/views.py
+++ books/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.views import ListView
+from .models import Books
 # Create your views here.
+
+class BookListView(ListView):
+    model = Books
+    template_name = 'book_list.html'
